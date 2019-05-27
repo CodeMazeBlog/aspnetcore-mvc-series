7c6ba91f3db8257d5d0d720ee2e6ff9da3383644
Vladimir Pecanac <vladimir.pecanac@gmail.com>
2019-05-27 08:54:19 +0200

    Part 1 of the series

---
+++ Part1/Models/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Book Title")]
+        [Required]
+        [StringLength(maximumLength: 20, ErrorMessage = "The Title length should be between 2 and 20.", MinimumLength = 2)]
+        public string Title { get; set; }
+
+        public string Genre { get; set; }
+
+        public List<string> Authors { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Range(1, 100)]
+        public decimal Price { get; set; }
+
+        [Display(Name = "Publish Date")]
+        [DataType(DataType.Date)]
+        public DateTime PublishDate { get; set; }
+    }
+}
---
+++ Part1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BookStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
