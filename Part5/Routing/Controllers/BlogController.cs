84f2e7e8c6825f6a5fd57a7f66f259439942395f
Vladimir Pecanac <vladimir.pecanac@gmail.com>
2019-06-23 11:43:20 +0200

    Part 5: Routing - Initial commit

---
+++ Part5/Routing/Controllers/BlogController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WorkingWithRouting.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Article(int articleId)
+        {
+            return View(articleId);
+        }
+    }
+}
---
+++ Part5/Routing/Controllers/BooksController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WorkingWithRouting.Controllers
+{
+    public class BooksController : Controller
+    {
+        [Route("")]
+        [Route("Home")]
+        [Route("Home/Index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("Home/Details/{id:int}")]
+        public IActionResult Details(int id)
+        {
+            ViewBag.Id = id;
+            return View();
+        }
+    }
+}
