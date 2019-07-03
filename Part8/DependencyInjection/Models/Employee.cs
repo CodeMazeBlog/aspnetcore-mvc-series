55a6dcbaea10463749f5ae3c38c071acc2636771
Vladimir Pecanac <vladimir.pecanac@gmail.com>
2019-07-04 00:25:50 +0200

    Part 8 - Unit Testing initial commit

---
+++ Part8/DependencyInjection/Models/Employee.cs
@@ -0,0 +1,8 @@
+﻿namespace DependencyInjection.Models
+{
+	public class Employee
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+	}
+}
---
+++ Part8/DependencyInjection/Models/Services/BooksLookupService.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace DependencyInjection.Models.Services
+{
+	public class BooksLookupService
+	{
+		public List<string> GetGenres()
+		{
+			return new List<string>()
+		{
+			"Fiction",
+			"Thriller",
+			"Comedy",
+			"Autobiography"
+		};
+		}
+	}
+}
