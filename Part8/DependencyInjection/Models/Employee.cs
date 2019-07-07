9ea89ebc8138a097912bf4a7c4eae30ec5133f15
Marinko <marinkospasojevic@gmail.com>
2019-07-07 19:53:39 +0200

    removed part 8

--- Part8/DependencyInjection/Models/Employee.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DependencyInjection.Models
-{
-	public class Employee
-	{
-		public int Id { get; set; }
-		public string Name { get; set; }
-	}
-}
--- Part8/DependencyInjection/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DependencyInjection.Models
-{
-	public class ErrorViewModel
-	{
-		public string RequestId { get; set; }
-
-		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-	}
-}
