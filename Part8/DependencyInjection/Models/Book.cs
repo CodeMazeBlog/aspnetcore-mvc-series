1c1b1fb2511e842db1a26c51499fbaea741d8042
Marinko <marinkospasojevic@gmail.com>
2019-07-14 10:13:38 +0200

    ASP.NET Core MVC series part 8 commit.

---
+++ Part8/DependencyInjection/Models/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DependencyInjection.Models
+{
+	public class Book
+	{
+		public int Id { get; set; }
+
+		[Display(Name = "Book Title")]
+		public string Title { get; set; }
+
+		public string Genre { get; set; }
+
+		[DataType(DataType.Currency)]
+		[Range(1, 100)]
+		public decimal Price { get; set; }
+
+		[Display(Name = "Publish Date")]
+		[DataType(DataType.Date)]
+		public DateTime PublishDate { get; set; }
+	}
+}
---
+++ Part8/DependencyInjection/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DependencyInjection.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
